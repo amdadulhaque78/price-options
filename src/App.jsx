063b2a53dabc22d8e56ec8a0fadf6eca70555ddb
amdadulhaque78 <amdadulhaque.ier.du@gmail.com>
2024-09-13 01:16:49 +0600

    project setup and daisy navbar created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-4xl bg-red-50'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
